ed5aea2866742de31eac3c0cd17499d3883f8c17
mariami <mariam.kavtaradze978@gmail.com>
2023-03-25 23:19:13 +0400

    lecture10

---
+++ script.js
@@ -0,0 +1,33 @@
+// //  function getFacebookProfile(1,(profile) => {})
+// //  .then(() => getFacebookProfile(profile.friends[0].id,(friendProfile)))
+// //  .then(() => getFacebookProfile(friendProfile.friends[0].id,(friendsFriend)))
+// //  .then (() => getFacebookProfile(friendsFriend.friends[0].id,(finalProfile)))
+// // .cath((err) => chconsole.log("Finally, i have all profiles"))
+   
+   
+// // async function getFacebookProfile(1,(profile) => {})
+// // await getFacebookProfile(profile.friends[0].id,(friendProfile))
+// // await getFacebookProfile(friendProfile.friends[0].id,(friendsFriend))
+// // await getFacebookProfile(friendsFriend.friends[0].id,(finalProfile))
+// // .cath((err) => chconsole.log("Finally, i have all profiles"))
+  
+  
+  
+// // async function X(){
+// //     let promise = new Promise((resolve,reject) => {
+// //         setTimeout(() => resolve("done!!"),100)
+// //     })
+// // }
+
+
+
+// function delay(ms){
+//     return new Promise((resolve) => {
+//         setTimeout(resolve,ms)
+        
+//     });
+// }
+
+// delay(1000).then(() => {
+//     console.log("Is it correct?")
+// })
